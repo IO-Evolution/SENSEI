fabe360f88fbc323303e5c9e04f8d0d58b5587f9
Elia Menoni <eliamenoni@emenoni.eu>
2023-06-20 18:20:25 +0000

    Ridenominazione componente principale progeto

--- src/SENSEI/urls.py
+++ src/SENSEI/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sensei project.
+URL configuration for SENSEI project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- src/SENSEI/wsgi.py
+++ src/SENSEI/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sensei project.
+WSGI config for SENSEI project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sensei.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SENSEI.settings")
 
 application = get_wsgi_application()
