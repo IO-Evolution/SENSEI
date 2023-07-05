be58c61e468a85eabc9fabca5bbc0a29df8f9ac7
Elia Menoni <eliamenoni@emenoni.eu>
2023-07-05 00:26:05 +0000

    API Test

--- src/FSE/views.py
+++ src/FSE/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+
+from .models import FSE_DOCUMENT
+from .serializers import DocumentSerializer
+
+class DocumentApiView(APIView):
+    def get(self, request, *args, **kwargs):
+        documents = FSE_DOCUMENT.objects.all()
+        serializer = DocumentSerializer(documents, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
