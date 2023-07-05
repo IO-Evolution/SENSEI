5eb9fa8c56c57f50caaaaa9b293f5e18fba77785
Elia Menoni <eliamenoni@emenoni.eu>
2023-07-05 18:29:32 +0000

    Componenti principali del documento

---
+++ src/FSE/CDA/components.py
@@ -0,0 +1,10 @@
+from elements import Attribute, Sub, Value
+
+class ClinicalDocument:
+    """ Clinical Document Class """
+    def __init__(self):
+        # ATTRS
+        #self.xsi = Attribute("xsi", {}, False)
+
+        # ELEMENTS
+        #self.sub = Sub(Prova, "ciao", {}, False)
