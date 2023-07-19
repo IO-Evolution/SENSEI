4fa2a2617aa490870113c31cea56d7b59f08fb4d
Elia Menoni <eliamenoni@emenoni.eu>
2023-07-19 11:43:00 +0000

    Fine stesura tipi atomici

--- src/FSE/CDA/data_types.py
+++ src/FSE/CDA/data_types.py
@@ -1,4 +1,5 @@
 import elements as el
+from exceptions import InvalidGivenValue, InvalidGivenSubelementData
 
 class Component:
     name: str
@@ -7,75 +8,188 @@ class Component:
 class BL_Boolean(Component):
     def __init__(self, name: str, data):
         self.name = name
-        self.value = el.Attribute("value", data)
+        self.value = el.Attribute("value", data, required=True)
 
 class II_InstanceIdentifier(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
-        self.root = el.Attribute("root", data)
+        self.root = el.Attribute("root", data, required=True)
         self.extension = el.Attribute("extension", data)
 
 class ED_EncapsulatedData(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
 
+        self.mediaType = el.Attribute("mediaType", data)
+        self.representation = el.Attribute("representation", data)
+
+        self.reference = el.Component(TEL_TelecomincationAddress, "reference", data)
+        self.thumbnail = el.Component(ED_EncapsulatedData, "thumbnail", data)
+
+        if self.reference is None and self.thumbnail is None:
+            self.text = data["text"]
+
+class ST_String(Component):
+    def __init__(self, name: str, data: str):
+        self.name = name
+
+        if type(data) != str:
+            raise InvalidGivenValue(f"{name} must be a string")
+        elif data == "":
+            raise InvalidGivenValue(f"{name} can not be empty string")
+        else:
+            self.text = data
+
 class CD_ConceptDescriptor(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
+
+        self.code = el.Attribute("code", data, required=True)
+        self.codeSystem = el.Attribute("codeSystem", data, required=True)
+        self.codeSystemVersion = el.Attribute("codeSystemVersion", data)
+        self.displayName = el.Attribute("displayName", data)
+        self.originalText = el.Component("originalText", ST_String, data)
+        self.translaction = el.Component("translaction", CD_ConceptDescriptor, data)
+        self.qualifier = el.Attribute("qualifier", data)
 
 class CE_CodedWithEquivalents(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
 
+        self.code = el.Attribute("code", data, required=True)
+        self.codeSystem = el.Attribute("codeSystem", data, required=True)
+        self.codeSystemVersion = el.Attribute("codeSystemVersion", data)
+        self.displayName = el.Attribute("displayName", data)
+        self.originalText = el.Component("originalText", ST_String, data)
+        self.translaction = el.Component("translaction", CD_ConceptDescriptor, data)
+
 class CV_CodedValue(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
+
+        self.code = el.Attribute("code", data, required=True)
+        self.codeSystem = el.Attribute("codeSystem", data, required=True)
+        self.codeSystemVersion = el.Attribute("codeSystemVersion", data)
+        self.displayName = el.Attribute("displayName", data)
+        self.originalText = el.Component("originalText", ST_String, data)
 
 class CS_CodedSimpleValue(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
 
+        self.code = el.Attribute("code", data, required=True)
+
 class TEL_TelecomincationAddress(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
 
+        self.value = el.Attribute("value", data, required=True)
+        self.use = el.Attribute("use", data)
+
+        """
+            value:
+            telephone (tel:)
+            fax (fax:)
+            email (mailto:)
+
+            use:
+            H: home
+            WP: Work
+            MC: mobile phone
+        """
+
+        self.useablePeriod = el.Component("useablePeriod", IVL_TS_IntervalOfTime, data)
+
 class AD_PostalAddress(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
+        
+        self.use = el.Attribute("use", data)
+
+        self.streetAddressLine = el.Component("streetAddressLine", ST_String, data)
+        self.city = el.Component("streetAddressLine", ST_String, data)
+        self.postalCode = el.Component("streetAddressLine", ST_String, data)
+        self.country = el.Component("streetAddressLine", ST_String, data)
+
+        """
+        https://wiki.hl7.de/index.php?title=HL7_CDA_Core_Principles#Postal_Address_.28AD.29
+        """
 
 class PN_PersonName(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
 
+        self.given = el.Component("given", ST_String, data)
+        self.family = el.Component("family", ST_String, data)
+        self.validTime = el.Component("validTime", IVL_TS_IntervalOfTime, data)
+
 class ON_OrganisationName(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
+
+        if type(data) != str:
+            raise InvalidGivenValue(f"{name} must be a string")
+        elif data == "":
+            raise InvalidGivenValue(f"{name} can not be empty string")
+        else:
+            self.text = data
+
+        # self.validTime = el.Component("validTime", IVL_TS_IntervalOfTime, data)
+        """ ????
+            <name>The old hospital
+                <validTime> 
+                    <high value="20111231235959"/>
+                </validTime>
+            </name>
+        """
 
 class INT_IntegerNumber(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
 
+        self.value = el.Attribute("value", data, required=True)
+
 class PQ_PhysicalQuantities(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
+
+        self.value = el.Attribute("value", data, required=True)
+        self.unit = el.Attribute("unit", data, required=True)
 
 class TS_PointInTime(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
 
-class IVL_Intervals(Component):
-    def __init__(self, name: str, data: dict):
-        self.name = name
+        self.value = el.Attribute("value", data, required=True)
+
+# class IVL_Intervals(Component):
+#     def __init__(self, name: str, data: dict):
+#         self.name = name
 
 class IVL_TS_IntervalOfTime(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
 
+        self.low = el.Component("low", INT_IntegerNumber, data)
+        self.high = el.Component("high", INT_IntegerNumber, data)
+        self.center = el.Component("center", INT_IntegerNumber, data)
+        self.width = el.Component("width", PQ_PhysicalQuantities, data)
+
 class IVL_PQ_IntervalOfPhysicalQuantities(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
 
+        self.low = el.Component("low", INT_IntegerNumber, data)
+        self.high = el.Component("high", INT_IntegerNumber, data)
+        self.center = el.Component("center", INT_IntegerNumber, data)
+
 class RTO_QTY_QTY_RatioOfQuantities(Component):
     def __init__(self, name: str, data: dict):
         self.name = name
+
+        self.numerator = el.Component("numerator", PQ_PhysicalQuantities, data)
+        if "unit" in data["denominator"]:
+            self.denominator = el.Component("denominator", PQ_PhysicalQuantities, data)
+        else:
+            self.denominator = el.Component("denominator", INT_IntegerNumber, data)
 
