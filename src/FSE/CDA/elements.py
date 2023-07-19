9bd15523176d24e16a77559930f00a65de7714f3
Elia Menoni <eliamenoni@emenoni.eu>
2023-07-19 14:52:51 +0000

    Correzioni bug e gestione liste

--- src/FSE/CDA/elements.py
+++ src/FSE/CDA/elements.py
@@ -2,7 +2,7 @@ from exceptions import InvalidGivenValue, InvalidGivenSubelementData
 
 class Attribute:
     """ XML Attributes Class """
-    def __new__(cls, name: str, data: dict, required: bool = True):
+    def __new__(cls, name: str, data: dict, required: bool = False):
         try:
             return data[name]
         except Exception as error:
@@ -13,19 +13,17 @@ class Attribute:
 
 class Component:
     """ XML Component Class """
-    def __new__(cls, className: type, name: str, data: dict, required: bool = True):
+    def __new__(cls, className: type, name: str, data: dict, required: bool = False, as_list: bool = True):
         try:
             if type(data[name]) == dict:
                 return className(name, data[name])
-            elif type(data[name]) == list:
-                return [className(name, data[name][i]) for i in data[name]]
+            elif type(data[name]) == list and as_list:
+                return [className(name, elem) for elem in data[name]]
             else:
                 raise InvalidGivenSubelementData(f"{name} of type {className.__class__} must be dict or list")
         except Exception as error:
-            if name not in data:
+            if required:
                 raise InvalidGivenSubelementData(f"Something went wrong generating {name} of type {className.__class__}") from error
-            elif required:
-                raise error
             return None
 
 # class Value:
