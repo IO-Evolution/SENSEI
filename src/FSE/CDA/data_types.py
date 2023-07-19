bc2573426bb0fdb537fd4db04fdb14aceeac131d
Elia Menoni <eliamenoni@emenoni.eu>
2023-07-19 14:52:26 +0000

    Correzioni bug

--- src/FSE/CDA/data_types.py
+++ src/FSE/CDA/data_types.py
@@ -1,23 +1,36 @@
 import elements as el
 from exceptions import InvalidGivenValue, InvalidGivenSubelementData
 
+
 class Component:
     name: str
     value: str
 
+
 class BL_Boolean(Component):
     def __init__(self, name: str, data):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
         self.value = el.Attribute("value", data, required=True)
 
+
 class II_InstanceIdentifier(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
         self.root = el.Attribute("root", data, required=True)
         self.extension = el.Attribute("extension", data)
 
+
 class ED_EncapsulatedData(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.mediaType = el.Attribute("mediaType", data)
@@ -27,60 +40,84 @@ class ED_EncapsulatedData(Component):
         self.thumbnail = el.Component(ED_EncapsulatedData, "thumbnail", data)
 
         if self.reference is None and self.thumbnail is None:
-            self.text = data["text"]
+            try:
+                self.text = data["text"]
+            except:
+                raise InvalidGivenValue("Text Needed")
+
 
 class ST_String(Component):
     def __init__(self, name: str, data: str):
-        self.name = name
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
 
-        if type(data) != str:
-            raise InvalidGivenValue(f"{name} must be a string")
-        elif data == "":
-            raise InvalidGivenValue(f"{name} can not be empty string")
-        else:
-            self.text = data
+        self.name = name
+        try:
+            self.text = data["text"]
+        except:
+            raise InvalidGivenValue("Text Needed")
+
 
 class CD_ConceptDescriptor(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.code = el.Attribute("code", data, required=True)
         self.codeSystem = el.Attribute("codeSystem", data, required=True)
         self.codeSystemVersion = el.Attribute("codeSystemVersion", data)
         self.displayName = el.Attribute("displayName", data)
-        self.originalText = el.Component("originalText", ST_String, data)
-        self.translaction = el.Component("translaction", CD_ConceptDescriptor, data)
+        self.originalText = el.Component(ST_String, "originalText", data)
+        self.translaction = el.Component(CD_ConceptDescriptor, "translaction", data)
         self.qualifier = el.Attribute("qualifier", data)
+
 
 class CE_CodedWithEquivalents(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.code = el.Attribute("code", data, required=True)
         self.codeSystem = el.Attribute("codeSystem", data, required=True)
         self.codeSystemVersion = el.Attribute("codeSystemVersion", data)
         self.displayName = el.Attribute("displayName", data)
-        self.originalText = el.Component("originalText", ST_String, data)
-        self.translaction = el.Component("translaction", CD_ConceptDescriptor, data)
+        self.originalText = el.Component(ST_String, "originalText", data)
+        self.translaction = el.Component(CD_ConceptDescriptor, "translaction", data)
+
 
 class CV_CodedValue(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.code = el.Attribute("code", data, required=True)
         self.codeSystem = el.Attribute("codeSystem", data, required=True)
         self.codeSystemVersion = el.Attribute("codeSystemVersion", data)
         self.displayName = el.Attribute("displayName", data)
-        self.originalText = el.Component("originalText", ST_String, data)
+        self.originalText = el.Component(ST_String, "originalText", data)
+
 
 class CS_CodedSimpleValue(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.code = el.Attribute("code", data, required=True)
 
+
 class TEL_TelecomincationAddress(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.value = el.Attribute("value", data, required=True)
@@ -100,10 +137,14 @@ class TEL_TelecomincationAddress(Component):
 
         self.useablePeriod = el.Component("useablePeriod", IVL_TS_IntervalOfTime, data)
 
+
 class AD_PostalAddress(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
-        
+
         self.use = el.Attribute("use", data)
 
         self.streetAddressLine = el.Component("streetAddressLine", ST_String, data)
@@ -115,24 +156,29 @@ class AD_PostalAddress(Component):
         https://wiki.hl7.de/index.php?title=HL7_CDA_Core_Principles#Postal_Address_.28AD.29
         """
 
+
 class PN_PersonName(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.given = el.Component("given", ST_String, data)
         self.family = el.Component("family", ST_String, data)
         self.validTime = el.Component("validTime", IVL_TS_IntervalOfTime, data)
 
-class ON_OrganisationName(Component):
-    def __init__(self, name: str, data: dict):
-        self.name = name
 
-        if type(data) != str:
-            raise InvalidGivenValue(f"{name} must be a string")
-        elif data == "":
-            raise InvalidGivenValue(f"{name} can not be empty string")
-        else:
-            self.text = data
+class ON_OrganisationName(Component):
+    def __init__(self, name: str, data: str):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
+        self.name = name
+        try:
+            self.text = data["text"]
+        except:
+            raise InvalidGivenValue("Text Needed")
 
         # self.validTime = el.Component("validTime", IVL_TS_IntervalOfTime, data)
         """ ????
@@ -143,31 +189,50 @@ class ON_OrganisationName(Component):
             </name>
         """
 
+
 class INT_IntegerNumber(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.value = el.Attribute("value", data, required=True)
 
+
 class PQ_PhysicalQuantities(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.value = el.Attribute("value", data, required=True)
         self.unit = el.Attribute("unit", data, required=True)
 
+
 class TS_PointInTime(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.value = el.Attribute("value", data, required=True)
 
 # class IVL_Intervals(Component):
 #     def __init__(self, name: str, data: dict):
-#         self.name = name
+# if data == {} or data is None:
+#  raise InvalidGivenValue("Empty Data Set")
+#
+# self.name = name
+
 
 class IVL_TS_IntervalOfTime(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.low = el.Component("low", INT_IntegerNumber, data)
@@ -175,16 +240,24 @@ class IVL_TS_IntervalOfTime(Component):
         self.center = el.Component("center", INT_IntegerNumber, data)
         self.width = el.Component("width", PQ_PhysicalQuantities, data)
 
+
 class IVL_PQ_IntervalOfPhysicalQuantities(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.low = el.Component("low", INT_IntegerNumber, data)
         self.high = el.Component("high", INT_IntegerNumber, data)
         self.center = el.Component("center", INT_IntegerNumber, data)
 
+
 class RTO_QTY_QTY_RatioOfQuantities(Component):
     def __init__(self, name: str, data: dict):
+        if data == {} or data is None:
+            raise InvalidGivenValue("Empty Data Set")
+
         self.name = name
 
         self.numerator = el.Component("numerator", PQ_PhysicalQuantities, data)
@@ -192,4 +265,3 @@ class RTO_QTY_QTY_RatioOfQuantities(Component):
             self.denominator = el.Component("denominator", PQ_PhysicalQuantities, data)
         else:
             self.denominator = el.Component("denominator", INT_IntegerNumber, data)
-
