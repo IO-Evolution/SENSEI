36292aeda468538aa29ebdaa7d780f63fad30d70
Elia Menoni <eliamenoni@emenoni.eu>
2023-08-11 12:34:59 +0000

    Ramo sbagliato

--- src/FSE/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import FSE_DOCUMENT
-
-admin.site.register(FSE_DOCUMENT)
--- src/FSE/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FseConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "FSE"
