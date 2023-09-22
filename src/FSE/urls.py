596cfc2113f4b494e32efdabebb44ff9fea82fa8
Elia Menoni <eliamenoni@emenoni.eu>
2023-09-22 11:52:07 +0200

    Introduzione Templating

--- src/FSE/urls.py
+++ src/FSE/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from .views import ClinicalDocumentGeneration
+from .views import ClinicalDocumentGeneration, ClinicalDocumentTemplate
 
 urlpatterns = [
-     path('generate/', ClinicalDocumentGeneration.as_view())
+     path('generate/', ClinicalDocumentGeneration.as_view()),
+     path('template/<int:doc_type>', ClinicalDocumentTemplate.as_view())
 ]
--- src/FSE/views.py
+++ src/FSE/views.py
@@ -1,8 +1,8 @@
-from PyCDA.Factory import XMLfactory as Factory
-from PyCDA.Components import ClinicalDocument as CD
-
-from rest_framework.views import APIView
+from PyClinicalDocumentArchitecture.Components import ClinicalDocument as CD
+from PyClinicalDocumentArchitecture.Factory import XMLfactory as Factory
 from django.http import HttpResponse
+from rest_framework.views import APIView
+
 
 class ClinicalDocumentGeneration(APIView):
 
@@ -10,3 +10,13 @@ class ClinicalDocumentGeneration(APIView):
         doc = CD.ClinicalDocument("ClinicalDocument", request.data)
         fact = Factory.XMLfactory(doc)
         return HttpResponse(fact.dict_to_xml(), content_type="application/xml")
+
+
+class ClinicalDocumentTemplate(APIView):
+
+    def get(self, request, doc_type):
+        from PyClinicalDocumentArchitecture.Templating.TemplateGenius import TemplateGenius, DOC_TYPE
+        import json
+        tg = TemplateGenius()
+        json_doc = json.dumps(tg.generate_template(DOC_TYPE(doc_type)))
+        return HttpResponse(json_doc, content_type="application/json")
