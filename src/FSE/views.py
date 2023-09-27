e01290ad4062d1f6f660a9604ca2fd56993243dd
Elia Menoni <eliamenoni@emenoni.eu>
2023-09-27 16:10:45 +0200

    Il JSON di log salvato in DB ora presenta una struttura corretta e il JSON di errore inviato al client riporta il codice del documento

--- src/FSE/views.py
+++ src/FSE/views.py
@@ -13,25 +13,26 @@ from .utils import get_client_ip
 class ClinicalDocumentGeneration(APIView):
 
     def post(self, request):
-        print(f"Incoming request from {get_client_ip(request)} for CDA generation service")
+        print(f"Incoming request from {get_client_ip(request)} for CDA generation service for document id={request.data['id']['extension']}")
         try:
             doc = CD.ClinicalDocument("ClinicalDocument", request.data)
             fact = Factory.XMLfactory(doc)
             xml_str = fact.dict_to_xml()
             try:
-                log = FSE_DOCUMENT_LOG(host=get_client_ip(request), json_code=request.data, xml_code=xml_str, doc_type=f"{request.data['code']['code']} {request.data['templateId']['extension']}", status="DONE")
+                log = FSE_DOCUMENT_LOG(host=get_client_ip(request), json_code=json.dumps(request.data), xml_code=xml_str, doc_type=f"{request.data['code']['code']} {request.data['templateId']['extension']}", status="DONE")
             except:
-                log = FSE_DOCUMENT_LOG(host=get_client_ip(request), json_code=request.data, xml_code=xml_str, doc_type="UNKNOWN", status="DONE")
+                log = FSE_DOCUMENT_LOG(host=get_client_ip(request), json_code=json.dumps(request.data), xml_code=xml_str, doc_type="UNKNOWN", status="DONE")
             log.save()
             return HttpResponse(xml_str, content_type="application/xml", status=200)
         except Exception as e:
             try:
-                log = FSE_DOCUMENT_LOG(host=get_client_ip(request), json_code=request.data, status="GENERATION_ERROR", doc_type=f"{request.data['code']['code']} {request.data['templateId']['extension']}", error=e)
+                log = FSE_DOCUMENT_LOG(host=get_client_ip(request), json_code=json.dumps(request.data), status="GENERATION_ERROR", doc_type=f"{request.data['code']['code']} {request.data['templateId']['extension']}", error=e)
             except:
-                log = FSE_DOCUMENT_LOG(host=get_client_ip(request), json_code=request.data, status="GENERATION_ERROR", doc_type="UNKNOWN", error=e)
+                log = FSE_DOCUMENT_LOG(host=get_client_ip(request), json_code=json.dumps(request.data), status="GENERATION_ERROR", doc_type="UNKNOWN", error=e)
             finally:
                 log.save()
                 return_err = {
+                    "document": request.data['id']['extension'],
                     "ts": log.date.__str__(),
                     "error": log.error.__str__()
                 }
